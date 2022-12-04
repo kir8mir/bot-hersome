0e59be21bc26d74b7ebabaa3c7641b22f83027c6
Cyrill Prudchenko <49589547+kir8mir@users.noreply.github.com>
2022-12-04 13:50:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,60 +24,60 @@ await doc.useServiceAccountAuth({
 
 let chatId;
 
-  app.post('/new-message', async (req, res) => {
-    const { message } = req.body
+app.post('/new-message', async (req, res) => {
+  const { message } = req.body
 
-    const messageText = message?.text?.toLowerCase()?.trim()
-    chatId = message?.chat?.id
-    if (!messageText || !chatId) {
-      return res.sendStatus(400)
-    }
-
-    await doc.loadInfo()
-    const sheet = doc.sheetsByIndex[0]
-    const rows = await sheet.getRows()
-    const dataFromSpreadsheet = rows.reduce((obj, row) => {
-      if (row.date) {
-        const todo = { text: row.text, done: row.done }
-        obj[row.date] = obj[row.date] ? [...obj[row.date], todo] : [todo]
-      }
-      return obj
-    }, {})
-
-    let responseText = 'I have nothing to say.'
-    if (messageText === 'joke') {
-      try {
-        const response = await axios(JOKE_API)
-        responseText = response.data.joke
-      } catch (e) {
-        console.log(e)
-        res.send(e)
-      }
-    } else if (/\d\d\.\d\d/.test(messageText)) {
-      responseText =
-        dataFromSpreadsheet[messageText] || 'You have nothing to do on this day.'
-    } else if (messageText === 'give') {
-      responseText =
-        dataFromSpreadsheet[messageText] || 'TEST TEST TEST'
-    }
-
-    try {
-      await axios.post(TELEGRAM_URI, {
-        chat_id: chatId,
-        text: responseText
-      })
-
-      res.send('Done')
-    } catch (e) {
-      console.log(e)
-      res.send(e)
-    }
-  })
+  const messageText = message?.text?.toLowerCase()?.trim()
+  chatId = message?.chat?.id
+  if (!messageText || !chatId) {
+    return res.sendStatus(400)
+  }
 
   setTimeout(await axios.post(TELEGRAM_URI, {
     chat_id: chatId,
     text: "Every 15 seconds"
   }), 15000)
+
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[0]
+  const rows = await sheet.getRows()
+  const dataFromSpreadsheet = rows.reduce((obj, row) => {
+    if (row.date) {
+      const todo = { text: row.text, done: row.done }
+      obj[row.date] = obj[row.date] ? [...obj[row.date], todo] : [todo]
+    }
+    return obj
+  }, {})
+
+  let responseText = 'I have nothing to say.'
+  if (messageText === 'joke') {
+    try {
+      const response = await axios(JOKE_API)
+      responseText = response.data.joke
+    } catch (e) {
+      console.log(e)
+      res.send(e)
+    }
+  } else if (/\d\d\.\d\d/.test(messageText)) {
+    responseText =
+      dataFromSpreadsheet[messageText] || 'You have nothing to do on this day.'
+  } else if (messageText === 'give') {
+    responseText =
+      dataFromSpreadsheet[messageText] || 'TEST TEST TEST'
+  }
+
+  try {
+    await axios.post(TELEGRAM_URI, {
+      chat_id: chatId,
+      text: responseText
+    })
+
+    res.send('Done')
+  } catch (e) {
+    console.log(e)
+    res.send(e)
+  }
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
