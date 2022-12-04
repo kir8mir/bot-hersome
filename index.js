076b8ff1ffcd2f88dfe3b7cda4e532299f584643
Cyrill Prudchenko <49589547+kir8mir@users.noreply.github.com>
2022-12-04 13:21:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,9 @@ app.post('/new-message', async (req, res) => {
   } else if (/\d\d\.\d\d/.test(messageText)) {
     responseText =
       dataFromSpreadsheet[messageText] || 'You have nothing to do on this day.'
+  }  else if (messageText === 'give') {
+    responseText =
+      dataFromSpreadsheet[messageText] || 'TEST TEST TEST'
   }
 
   try {
